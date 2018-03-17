408f2c8dfa9254ab7438e1224f0fe825a2626c84
LaughingManDotEXE <35981510+LaughingManDotEXE@users.noreply.github.com>
2018-03-17 12:09:08 -0500

    Udacity reviewed
    Updated after notes from Udacity. Verified against PEP8

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,33 +1,34 @@
 import fresh_tomatoes
 import media
 
-#objects of Class Movie
-sombra_short = media.Movie("Sombra - Infiltration", "Sombra is introduced to the Overwatch Universe",
-					"sombra_art.jpg",
-					"https://www.youtube.com/watch?v=tU7jskA1zWI")
-doomfist_short = media.Movie("Doomfist - Animated Short", "Doomfist destroys the heroes of Overwatch",
-					"doomfist_art.jpg",
-					"https://www.youtube.com/watch?v=vaZfZFNuOpI")
-moira_short = media.Movie("Moira - Origin Story", "Moira talks about why she joined Blackwatch",
-					"moira_art.png",
-					"https://www.youtube.com/watch?v=6ETybQd4uRE")
-
-mei_short = media.Movie("Mei - Rise and Shine", "Mei's path from scientist to Overwatch member",
-					"mei_art.jpg",
-					"https://www.youtube.com/watch?v=8tjcm_kI0n0")
-					
-winston_short = media.Movie("Winston - Recall", "Winston has a huge decision to make",
-					"winston_art.jpg",
-					"https://www.youtube.com/watch?v=sB5zlHMsM7k")
-overwatch_short = media.Movie("Overwatch - Trailer", "Overwatch debut trailer",
-					"overwatch_art.jpg",
-					"https://www.youtube.com/watch?v=K_m8fcJb5tE")
-
-
-
-movies = [sombra_short, doomfist_short, moira_short, mei_short, winston_short, overwatch_short]
+# objects of Class Movie
+sombra_short = media.Movie("Sombra - Infiltration",
+                           "Sombra is introduced to the Overwatch Universe",
+                           "sombra_art.jpg",
+                           "https://www.youtube.com/watch?v=tU7jskA1zWI")
+doomfist_short = media.Movie("Doomfist - Animated Short",
+                             "Doomfist destroys the heroes of Overwatch",
+                             "doomfist_art.jpg",
+                             "https://www.youtube.com/watch?v=vaZfZFNuOpI")
+moira_short = media.Movie("Moira - Origin Story",
+                          "Moira talks about why she joined Blackwatch",
+                          "moira_art.png",
+                          "https://www.youtube.com/watch?v=6ETybQd4uRE")
+mei_short = media.Movie("Mei - Rise and Shine",
+                        "Mei's path from scientist to Overwatch member",
+                        "mei_art.jpg",
+                        "https://www.youtube.com/watch?v=8tjcm_kI0n0")
+winston_short = media.Movie("Winston - Recall",
+                            "Winston has a huge decision to make",
+                            "winston_art.jpg",
+                            "https://www.youtube.com/watch?v=sB5zlHMsM7k")
+overwatch_short = media.Movie("Overwatch - Trailer",
+                              "Overwatch debut trailer",
+                              "overwatch_art.jpg",
+                              "https://www.youtube.com/watch?v=K_m8fcJb5tE")
+movies = [sombra_short, doomfist_short, moira_short,
+          mei_short, winston_short, overwatch_short]
 fresh_tomatoes.open_movies_page(movies)
-
-#commented out due to not being needed for this part of project
-#print(media.Movie.VALID_RATINGS)
-#print(media.Movie.__doc__)
+# commented out due to not being needed for this part of project
+# print(media.Movie.VALID_RATINGS)
+# print(media.Movie.__doc__)
--- media.py
+++ media.py
@@ -1,13 +1,18 @@
 import webbrowser
 
+
+# class Movie is made so further objects of Movie can be created
 class Movie():
-	"""here is some documentations"""
-	#VALID_RATINGS = ["E", "T", "M"]
-	def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
-		self.title = movie_title
-		self.storyline = movie_storyline
-		self.poster_image_url = poster_image
-		self.trailer_youtube_url = trailer_youtube
-		
-	def show_trailer(self):
-		webbrowser.open(self.trailer_youtube_url)
+    """here is some documentations"""
+	# VALID_RATINGS = ["E", "T", "M"]
+    # these four are attributes of a movie,
+    # and self is needed to define the init
+    def __init__(self, movie_title, movie_storyline,
+                 poster_image, trailer_youtube):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
