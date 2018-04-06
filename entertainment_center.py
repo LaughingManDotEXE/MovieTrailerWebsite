8180a3bbfe18be327e883e06b1a6147dc039765b
LaughingManDotEXE <35981510+LaughingManDotEXE@users.noreply.github.com>
2018-04-06 16:09:41 -0500

    Changes Made 4/6
    Added more comments due to feedback

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,34 +1,40 @@
 import fresh_tomatoes
 import media
 
-# objects of Class Movie
+# objects of Class Movie 
+# Sombra : Title, short description, poster image, and youtube link
 sombra_short = media.Movie("Sombra - Infiltration",
                            "Sombra is introduced to the Overwatch Universe",
                            "sombra_art.jpg",
                            "https://www.youtube.com/watch?v=tU7jskA1zWI")
+# Doomfist : Title, short description, poster image, and youtube link
 doomfist_short = media.Movie("Doomfist - Animated Short",
                              "Doomfist destroys the heroes of Overwatch",
                              "doomfist_art.jpg",
                              "https://www.youtube.com/watch?v=vaZfZFNuOpI")
+# Moira : Title, short description, poster image, and youtube link
 moira_short = media.Movie("Moira - Origin Story",
                           "Moira talks about why she joined Blackwatch",
                           "moira_art.png",
                           "https://www.youtube.com/watch?v=6ETybQd4uRE")
+# Mei : Title, short description, poster image, and youtube link
 mei_short = media.Movie("Mei - Rise and Shine",
                         "Mei's path from scientist to Overwatch member",
                         "mei_art.jpg",
                         "https://www.youtube.com/watch?v=8tjcm_kI0n0")
+# Winston : Title, short description, poster image, and youtube link
 winston_short = media.Movie("Winston - Recall",
                             "Winston has a huge decision to make",
                             "winston_art.jpg",
                             "https://www.youtube.com/watch?v=sB5zlHMsM7k")
+# Overwatch : Title, short description, poster image, and youtube link
 overwatch_short = media.Movie("Overwatch - Trailer",
                               "Overwatch debut trailer",
                               "overwatch_art.jpg",
                               "https://www.youtube.com/watch?v=K_m8fcJb5tE")
+# movies creates an array and orders the objects passed to media
 movies = [sombra_short, doomfist_short, moira_short,
           mei_short, winston_short, overwatch_short]
+# fresh_tomatoes function open_movies_page called to display page
 fresh_tomatoes.open_movies_page(movies)
-# commented out due to not being needed for this part of project
-# print(media.Movie.VALID_RATINGS)
-# print(media.Movie.__doc__)
+
--- media.py
+++ media.py
@@ -1,18 +1,16 @@
 import webbrowser
 
 
-# class Movie is made so further objects of Movie can be created
 class Movie():
-    """here is some documentations"""
-	# VALID_RATINGS = ["E", "T", "M"]
-    # these four are attributes of a movie,
-    # and self is needed to define the init
+    # class Movie is made so further objects of Movie can be created
     def __init__(self, movie_title, movie_storyline,
                  poster_image, trailer_youtube):
+        # these four are attributes of a movie,
+        # and self is needed to define the init
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
-
     def show_trailer(self):
+        # show_trailer is defined to create a popup window with video
         webbrowser.open(self.trailer_youtube_url)
